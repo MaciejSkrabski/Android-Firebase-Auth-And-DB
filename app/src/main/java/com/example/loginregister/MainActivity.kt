79b79959e60b35f5a50264cd7efd90105434612e
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-05-20 18:50:23 +0200

    Firebase linking

--- app/src/main/java/com/example/loginregister/MainActivity.kt
+++ app/src/main/java/com/example/loginregister/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.loginregister
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var txt: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txt = testing
+        mAuth = FirebaseAuth.getInstance()
+
+    }
+
+    override fun onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = mAuth.currentUser
+        txt.text = currentUser.toString()
+
     }
 }

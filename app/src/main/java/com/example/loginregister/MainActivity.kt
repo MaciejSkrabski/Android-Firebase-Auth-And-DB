b4f63e1b882e4b7ad4370a2481b1c9c159e5d7b8
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-05-21 01:59:56 +0200

    two activities

--- app/src/main/java/com/example/loginregister/MainActivity.kt
+++ app/src/main/java/com/example/loginregister/MainActivity.kt
@@ -1,30 +1,96 @@
 package com.example.loginregister
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var nav: NavController
-    lateinit var auth: FirebaseAuth
+    private lateinit var mail: EditText
+    private lateinit var pwd: EditText
+    private lateinit var reg: Button
+    private lateinit var log: TextView
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
         auth = Firebase.auth
-        Log.d("user", "${auth.currentUser.toString()}")
 
-        nav = Navigation.findNavController(this, R.id.fragment_nav)
+        mail = email
+        pwd = password
+        reg = register
+        log = login
 
+
+        reg.setOnClickListener { Log.d("inputs", "${mail.text}, ${pwd.text}") }
+        log.setOnClickListener {
+            Log.d("login", "try to start login activity")
+            val intent = Intent (this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        Log.i("currentUser", "$currentUser")
+        //updateUI(currentUser)
+    }
+
+    fun createAccount(email:String, password:String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("createuser", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("createuser", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    fun login(email:String, password:String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("login", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("login", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                    // ...
+                }
+
+                // ...
+            }
     }
 }

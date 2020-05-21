3bdd4f57cf115a3e0e1f4a99e8cb1f9bc3aa2ec2
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-05-21 05:56:06 +0200

    Some navigation magic

--- app/src/main/java/com/example/loginregister/FragmentNavigationActivity.kt
+++ app/src/main/java/com/example/loginregister/FragmentNavigationActivity.kt
@@ -2,11 +2,24 @@ package com.example.loginregister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_fragment_navigation.*
 
 class FragmentNavigationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_navigation)
+
+        val bottomNavigationView = bnv
+        val navController = findNavController(fragment.id)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.books, R.id.films, R.id.games))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
